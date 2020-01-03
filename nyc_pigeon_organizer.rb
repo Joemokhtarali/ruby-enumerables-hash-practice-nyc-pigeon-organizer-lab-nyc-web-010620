d48882b7874a6a9cc65265eb4108efaf02fd9ea0
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-03 03:45:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,7 +19,7 @@ def nyc_pigeon_organizer(data)
       end 
     end 
   end 
-  binding.pry
+  # binding.pry
   # data = { 
   #       :color => {
   #         :purple => ["Theo", "Peter Jr.", "Lucky"],
